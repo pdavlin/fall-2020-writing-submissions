2f8939ce87ad965682c6db096e740cb13ccfd451
Patrick Davlin <patrickdavlin@Patricks-iMac.local>
2020-09-21 10:17:36 -0500

    ✨ Add code listing

---
+++ csci-8110/hw-1/HW1_code.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+import time
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.keras import backend as K
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
+
+# In[2]:
+
+data = data = tf.keras.datasets.cifar10
+(x_train, _), (x_test, _) = data.load_data()
+x_train = x_train.astype('float32')/255
+x_test = x_test.astype('float32')/255
+input_img = Input(shape=(32,32,3))
+print('Number of testing images:', x_test.shape)
+
+# In[3]:
+
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(input_img)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+encoded = MaxPooling2D((2, 2), padding='same')(x)
+
+# In[4]:
+
+x = Conv2D(16, (3, 3), activation='relu', padding='same')(encoded)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = UpSampling2D((2, 2))(x)
+decoded = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(x)
+
+
+# In[5]:
+
+autoencoder = Model(input_img, decoded)
+autoencoder.compile(optimizer='adam', loss='mae')
+
+# In[1]:
+
+autoencoder.fit(x_train, x_train, 
+                epochs=1,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_test, x_test))
+
+# In[7]:
+
+encoded_imgs = autoencoder.predict(x_test)
+decoded_imgs = autoencoder.predict(encoded_imgs)
+
+# In[8]:
+
+time.sleep(3)
+
+# In[9]:
+
+n = 10
+plt.figure(figsize=(20,4))
+for i in range(n):
+    ax = plt.subplot(2, n, i+1)
+    plt.imshow(x_test[i].reshape(32,32,3))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(32,32,3))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
+plt.savefig('storage/test.png')
+
+# In[10]:
+
+n = 10
+plt.figure(figsize=(20,4))
+for i in range(n):
+    ax = plt.subplot(2, n, i+1)
+    plt.imshow(x_test[i].reshape(32,32,3))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(32,32,3))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.savefig('storage/4k_enc.png')
