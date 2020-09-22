e46c74115dd97de3cebb49a7683ea58880b85ca9
Patrick Davlin <patrick@davlin.io>
2020-09-22 13:19:57 -0500

    Update HW1_code.py

--- csci-8110/hw-1/HW1_code.py
+++ csci-8110/hw-1/HW1_code.py
@@ -22,20 +22,20 @@ print('Number of testing images:', x_test.shape)
 
 # In[3]:
 
-x = Conv2D(256, (3, 3), activation='relu', padding='same')(input_img)
+x = Conv2D(64, (3, 3), activation='relu', padding='same')(input_img)
 x = MaxPooling2D((2, 2), padding='same')(x)
-x = Conv2D(512, (3, 3), activation='relu', padding='same')(x)
+x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
 x = MaxPooling2D((2, 2), padding='same')(x)
-x = Conv2D(1024, (3, 3), activation='relu', padding='same')(x)
+x = Conv2D(256, (3, 3), activation='relu', padding='same')(x)
 encoded = MaxPooling2D((2, 2), padding='same')(x)
 
 # In[4]:
 
-x = Conv2D(1024, (3, 3), activation='relu', padding='same')(encoded)
+x = Conv2D(256, (3, 3), activation='relu', padding='same')(encoded)
 x = UpSampling2D((2, 2))(x)
-x = Conv2D(512, (3, 3), activation='relu', padding='same')(x)
+x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
 x = UpSampling2D((2, 2))(x)
-x = Conv2D(256, (3, 3), activation='relu', padding='same')(x)
+x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
 x = UpSampling2D((2, 2))(x)
 decoded = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(x)
 
@@ -47,7 +47,7 @@ autoencoder.compile(optimizer='adam', loss='mae')
 # In[6]:
 
 autoencoder.fit(x_train, x_train, 
-                epochs=50,
+                epochs=500,
                 batch_size=256,
                 shuffle=True,
                 validation_data=(x_test, x_test))
